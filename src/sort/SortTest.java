5a53ba4ca83851ceb39f273fb58cbc59c531fbc3
chenming <lichenming_hs@163.com>
2021-11-07 22:04:08 +0800

    ..

--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -11,14 +11,101 @@ public class SortTest {
 
         printArray(arr1);
         System.out.println("==============================================");
-        quickSort(arr1, 0, arr1.length - 1);
+//        dualPivotQuickSort(arr1, 0, arr1.length - 1);
+        heapSort(arr1);
         printArray(arr1);
 
     }
 
 
     /**
+     * 堆排序
+     *
+     * @param a
+     */
+    public static void heapSort(int[] a) {
+        for (int end = a.length - 1; end > 0; end--) {
+            //最后一个带有叶子节点的父节点坐标 （start+end）/2 向上取整 减一
+            int lastFarther = end % 2 != 0 ? end / 2 : end / 2 - 1;
+
+            for (int father = lastFarther; father >= 0; father--) {
+                //取左节点：2*n +1
+                int left = father * 2 + 1;
+                //取右节点：2*n +2
+                int right = father * 2 + 2;
+                if (right <= end && a[right] > a[father]) {
+                    swap(a, right, father);
+                }
+                if (a[left] > a[father]) {
+                    swap(a, left, father);
+                }
+            }
+            swap(a, 0, end);
+        }
+    }
+
+
+    /**
+     * 双轴快速排序
+     *
+     * @param a
+     * @param start
+     * @param end
+     */
+    public static void dualPivotQuickSort(int[] a, int start, int end) {
+        if (start > end) return;
+        if (a[start] > a[end]) {
+            swap(a, start, end);
+        }
+        int pivot1 = a[start];
+        int pivot2 = a[end];
+
+        int left = start;
+        int right = end;
+        int k = start + 1;
+
+        OUT_LOOP:
+        //定义外层循环的名称，可方便直接跳出外层循环
+        while (k < right) {
+            if (a[k] < pivot1) {
+                //轴的位置不变，扩大左轴区域大小
+                ++left;
+                swap(a, left, k);
+                k++;
+            } else if (a[k] < pivot2) {
+                //位置处于两个轴中间
+                k++;
+            } else {
+                //右轴位置不变，扩大右轴区域大小，所以要一直找到比右轴小的数
+                while (a[--right] > pivot2) {
+                    if (right <= k) break OUT_LOOP;
+                }
+                swap(a, right, k);
+
+                //第二种处理，如果a[right]比pivot1小，就多交换一次 省去一次循环
+//                if (a[right] >= pivot1 && a[right] <= pivot2) {
+//                    swap(a, right, k);
+//                    k++;
+//                } else {
+//                    ++left;
+//                    swap(a, right, k);
+//                    swap(a, left, k);
+//                    k++;
+//                }
+            }
+        }
+        swap(a, start, left);  //分别移动两个轴的位置
+        swap(a, right, end);
+        printArray(a);
+
+        dualPivotQuickSort(a, start, left - 1);
+        dualPivotQuickSort(a, left + 1, right - 1);
+        dualPivotQuickSort(a, right + 1, end);
+    }
+
+    /**
      * 快速排序  O(n * log n)
+     *
      * @param a
      * @param start
      * @param end
@@ -40,11 +127,9 @@ public class SortTest {
         }
         //将轴移动到中间
         swap(a, left, end);
-        quickSort(a, start, left-1);
+        quickSort(a, start, left - 1);
         quickSort(a, left + 1, end);
     }
-
-
 
 
     /**
@@ -123,7 +208,7 @@ public class SortTest {
 
 
     public static void swap(int[] a, int i, int j) {
-        if(i == j ) return;
+        if (i == j) return;
         a[i] = a[i] ^ a[j];
         a[j] = a[i] ^ a[j];
         a[i] = a[i] ^ a[j];
